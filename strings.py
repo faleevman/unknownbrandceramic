fe285896b9210c0743ec975b300dcccbc7ce4c95
faleevman <91379320+faleevman@users.noreply.github.com>
2021-09-26 11:43:26 +0000

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,41 @@
+start = """
+Welcome to  UB!
+NFT AD Marketplace
+"""
+
+select_role = """
+Who are you?
+"""
+
+profile = """
+My info:
+Role: {}
+Balance: {} ETH
+"""
+
+nftPage = """
+{} - by {}
+
+Description: {}
+
+Price per place: {}
+Price per day: {}
+"""
+
+
+lk_seller = """
+You have {} NFTs
+Earned total: {} ETH
+"""
+
+lk_client = """
+My ads:
+"""
+
+client_p = """
+Name: {}
+Description: {}
+Price per placement: {} ETH
+Price per day: {} ETH
+Expires in: {}
+"""
