c8485cfc5164c8337b191f81133dcdb1cc97f3e3
faleevman <91379320+faleevman@users.noreply.github.com>
2021-09-26 12:24:36 +0000

    Update ipfs.py

--- telegrammBot/ipfs.py
+++ telegrammBot/ipfs.py
@@ -1,8 +1,13 @@
 import requests
 import shutil
+import uuid
 url = "https://ipfs.io/ipfs/{}"
 
 def get_ipfs(hash):
     print("Hash", hash)
-    response = requests.get(url.format(hash), stream=True)
-    return response.raw
+    h = name = uuid.uuid4().hex
+    r = requests.get(url.format(hash), stream=True)
+    with open("12345.png", 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+    return "12345" + ".png"
